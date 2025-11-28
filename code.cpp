a6004a4322d33c5679059bdfdb9ee7d6268ad83b
Rajni Meghwal <meghwalrajni27@gmail.com>
2025-11-28 22:04:13 +0530

    Initial commit

---
+++ code.cpp
@@ -0,0 +1,163 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    // int a = 42; 
+    // cout << a << endl;
+
+    // char grade = 'a'; 
+    // cout << grade << endl;
+
+    // float pi = 3.14f;
+    // cout << pi << endl;
+
+    // bool isCodingFun = true;
+    // cout << isCodingFun << endl;
+
+    // double largeNumber = 100.99;
+    // cout << largeNumber << endl;
+
+    // cout << "hieyyyyy\nworlddd"<< endl;
+
+    // char grade = 'A'; // type conversion from char to int
+    // int value = grade;
+    // cout << grade << " is " << value << endl;
+
+    // double pi = 100.99; // type casting from double to int
+    // int intPi = (int)pi;
+    // cout << pi << " is " << intPi << endl;
+
+
+
+
+
+
+    // int age;
+    // cout << "Enter the age : ";
+    // cin >> age;
+    // cout << "Your age is " << age << endl;
+
+
+
+    // double price;
+    // cout << "Enter the price : ";
+    // cin >> price;
+    // cout << "The price is " << price << endl;
+
+
+    // Arithmetic Operators
+    // int a = 11, b = 5;
+
+    // cout << "sum " << (a + b)<< endl;
+    // cout << "difference " << (a - b)<< endl;
+    // cout << "product " << (a * b)<< endl;
+    // cout << "division " << (a / b)<< endl;
+    // cout << "modulus " << (a % b)<< endl;
+
+
+
+    // cout << (5 / (double)2) << endl;
+
+
+    //relational operators
+    // cout << (5 > 3) << endl;  // true -> 1
+    // cout << (5 < 3) << endl;  // false -> 0
+    // cout << (5 >= 3) << endl; // true -> 1
+    // cout << (5 <= 3) << endl; // false -> 0
+    // cout << (5 == 3) << endl; // false -> 0
+    // cout << (5 != 3) << endl; // true -> 1
+
+    //logical operators
+    // cout << (5 > 3 && 2 < 4) << endl;  // true && true -> 1
+    // cout << (5 > 3 && 2 > 4) << endl;  // true && false -> 0
+    // cout << (5 > 3 || 2 > 4) << endl;  // true || false -> 1
+    // cout << (5 > 3 || 2 < 4) << endl;  // true || true -> 1
+    // cout << !(5 > 3) << endl;           // !true -> false -> 0
+
+
+    //unary operators
+    // int a = 10;
+
+    // int b = a++; // post-increment   kaam -> update
+    // cout << "a: " << a << ", b: " << b << endl;
+
+    // int c = ++a; // pre-increment   update -> kaam
+    // cout << "a: " << a << ", c: " << c << endl;
+
+
+
+    // int a = 10;
+
+    // int b = a--; // post-decrement   kaam -> update
+    // cout << "a: " << a << ", b: " << b << endl;
+
+    // int c = --a; // pre-decrement   update -> kaam
+    // cout << "a: " << a << ", c: " << c << endl;
+
+
+
+    //conditional statements and loops
+
+    // int n = -45 ;
+
+    // if (n >= 0) {
+    //     cout << " n is a positive number." << endl;
+    // } else {
+    //     cout <<" n is a negative number." << endl;
+    // }
+
+
+
+    
+    // int age ;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // if (age >= 18) {
+    //     cout << "You can vote." << endl;
+    // } else {
+    //     cout <<"You are not eligible to vote." << endl;
+    // }
+
+
+    // int n ;
+    // cout << "Enter number: ";
+    // cin >> n;
+
+    // if (n % 2 == 0) {
+    //     cout << "even." << endl;
+    // } else {
+    //     cout <<"odd." << endl;
+    // }
+
+
+
+    int marks
+
+ 
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+    return 0;
+}
+
+
+//boilerplate code
