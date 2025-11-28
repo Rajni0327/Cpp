ff0900dc7ac2bf8e954deb520a27de41d05666b2
Rajni Meghwal <meghwalrajni27@gmail.com>
2025-11-28 17:54:29 +0530

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,48 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    // int a = 42; 
+    // cout << a << endl;
+
+    // char grade = 'a'; 
+    // cout << grade << endl;
+
+    // float pi = 3.14f;
+    // cout << pi << endl;
+
+    // bool isCodingFun = true;
+    // cout << isCodingFun << endl;
+
+    // double largeNumber = 100.99;
+    // cout << largeNumber << endl;
+
+    // cout << "hieyyyyy\nworlddd"<< endl;
+
+    // char grade = 'A'; // type conversion from char to int
+    // int value = grade;
+    // cout << grade << " is " << value << endl;
+
+    // double pi = 100.99; // type casting from double to int
+    // int intPi = (int)pi;
+    // cout << pi << " is " << intPi << endl;
+
+
+
+    int age;
+    cout << "Enter the age : ";
+    cin >> age;
+    cout << "Your age is " << age << endl;
+
+ 
+
+
+
+
+
+
+    return 0;
+}
+
+
+//boilerplate code
